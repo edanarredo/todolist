155772c6eb9990c3f7c77286c1be4a4e8c4803d5
Daniel Arredondo <edanielarredondo@gmail.com>
2021-01-26 12:24:39 -0600

    add appending to list and css

--- src/app.js
+++ src/app.js
@@ -4,18 +4,33 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.get("/", function(req, res) {
+let items = ["buy food", "cook food", "eat food"];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("../public"));
+
+app.get("/", (req, res) => {
 
    const today = new Date();
-   const currentDay = today.getDay();
+   fullDateConfig = { 
+      weekday: 'long', 
+      year: 'numeric', 
+      month: 'long', 
+      day: 'numeric' 
+   
+   };
 
-   if (currentDay === 6 || currentDay === 0)
-      res.send("<h1>Happy weekend!</h1>");
-   else  
-      res.send("<h1>Boooo, weekday - big yikes</h1>");
+   const date = today.toLocaleDateString("en-US", fullDateConfig);
+   res.render('list', {kindOfDay : date, newListItems: items});
 
 });
 
-app.listen(3000, function() {
-   console.log("Server started on port 3000");
+app.post("/", (req, res) => {
+   let newItem = req.body.newItem;
+   items.push(newItem);
+   res.redirect("/");
 });
+
+app.listen(3000, () => console.log('Website running on port 3000.'));
